b7d320f0768d45fe5a125f9a558c541abd460c98
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-02-13 16:37:05 -0500

    Adds exchange setup for producers

--- MessageBroker/MessageBrokerObjectLibrary.php
+++ MessageBroker/MessageBrokerObjectLibrary.php
@@ -40,11 +40,6 @@ class MessageBroker
 
       // Connect
       $this->connection = new AMQPConnection($credentials['host'], $credentials['port'], $credentials['username'], $credentials['password']);
-      
-      $bla = TRUE;
-      if ($bla) {
-        $bla = FALSE;
-      }
 
       if(!$this->connection->isConnected()) {
         throw new Exception('Connection failed.');
@@ -56,50 +51,143 @@ class MessageBroker
    * produceTransactional - called to trigger production of a transactional
    * entry in an exchange / queue.
    *
-   * @param array $param
-   *  Values used to generate a production entry
+   * @param array $data
+   *  Message data to be passed through broker
    * 
    */
-  public function produceTransactional($param) {
-    
+  public function produceTransactional($data) {
+
 $bla = TRUE;
 if ($bla) {
   $bla = TRUE;
 }
 
+    $exchangeName = getenv("TRANSACTIONAL_EXCHANGE");
+
+    // Collect RabbitMQ connection details
     $connection = $this->connection;
     $channel = $connection->channel();
 
-    $queueName = getenv("TRANSACTIONAL_QUEUE");
-
     // Exchange
-    // Use common function to share between producer and consumer
-    // $channel->exchange_declare('direct_logs', 'direct', false, false, false);
-
-    // @todo: Move to function to allow producers and consumers to create the
-    // same queue. Declare queue as durable, pass third parameter to
-    // queue_declare as true. Needs to be set to true to both the producer and
-    // consumer queue_declare
-    // If the queue name is not set (using temporary random queue name), use
-    // list($queue_name, ,) = queue_declare(
-    $channel->queue_declare($queueName, false, true, false, false);
-
-    // https://www.rabbitmq.com/tutorials/tutorial-four-php.html
-    // $channel->queue_bind($queue_name, 'direct_logs', $severity);
+    $channel = getExchange($exchangeName, $channel);
 
     // Mark messages as persistent by setting the delivery_mode = 2 message property
     // Supported message properties: https://github.com/videlalvaro/php-amqplib/blob/master/doc/AMQPMessage.md
-    $payload = new AMQPMessage($param, array('delivery_mode' => 2));
+    $payload = new AMQPMessage($data, array('delivery_mode' => 2));
 
-    $channel->basic_publish($payload, '', $queueName);
-    // Publish with exchange
-    // $channel->basic_publish($msg, 'direct_logs', $severity);
+    // @todo: Exchange keys based on the transaction type:
+    // - user.password_reset.transactional
+    // - user.signup.transactional
+    // - campaign.signup.transactional
+    // - campaign.report_back.transactional
+    // $keys = setKeys($data);
+    $keys = 'campaign.signup.transactional';
+
+    $channel->basic_publish($payload, $exchangeName, $keys);
 
     $channel->close();
     $connection->close();
 
   }
-  
+
+  /**
+   * getExchange - common create exchange functionality used to ensure exchange
+   * settings are the same for both producers and consumers. A producer will
+   * never communicate with a queue directly, it's always through an exchange.
+   *
+   * @param string $exchangeName
+   *  Name of the exchange that will bind to a queue
+   *
+   * @param object $channel
+   *  The channel connection that the queue should use.
+   *
+   * @return object $channel
+   *
+   */
+  protected function getExchange($exchangeName, $channel) {
+
+    $exchange_options = array(
+      'passive' => FALSE,
+      'durable' => TRUE,
+      'auto_delete' => FALSE,
+      'nowait' => TRUE
+    );
+
+    // $ rabbitmqctl list_exchanges
+    $channel->exchange_declare($exchangeName,
+                              'direct',
+                              $exchange_options['passive'],
+                              $exchange_options['durable'],
+                              $exchange_options['auto_delete']);
+
+    return $channel;
+  }
+
+  /**
+   * consumeTransactional - called to setup consumer script of transactional
+   * message entries in the queue.
+   */
+  public function consumeTransactional() {
+
+    // Stub out consume transactional
+
+  }
+
+  /**
+   * getQueue - common create queue functionality used to ensure queue settings
+   * are the same for both producers and consumers. If the queue already exsists
+   * the details of the queue will be return.
+   *
+   * @param string $queueName
+   *  Name of the queue
+   *
+   * @param object $channel
+   *  The channel connection that the queue should use.
+   *
+   * @param array $param
+   *  Future use, possible flag for different queues.
+   *
+   * @return object
+   *
+   */
+  protected function getQueue(string $queueName, $channel, $param) {
+
+    /*
+     * passive:
+     *
+     * durable: Keep queue even if RabbitMQ  is shut down. Note that messages
+     * must also be marked as durable in order for messages in a durable queue
+     * to survive a queue restart.
+     *
+     * exclusive:
+     *
+     * auto_delete:
+     */
+
+    $queue_options = array(
+      'passive'     => FALSE,
+      'durable'     => TRUE,
+      'exclusive'   => FALSE,
+      'auto_delete' => FALSE,
+
+      'nowait'      => 'false',
+      'arguments'   => NULL,
+      'ticket'      => NULL,
+    );
+
+    // Old produceTransactional call
+    // Must be the same for both producer and consumer
+    // $channel->queue_declare($queueName, false, true, false, false);
+
+    $channel->queue_declare($queueName,
+      $queue_options['passive'],
+      $queue_options['durable'],
+      $queue_options['exclusive'],
+      $queue_options['auto_delete']);
+
+    return $channel;
+  }
+
   /**
    * queueStatus - called to gather details on a specific queue status.
    *
@@ -108,9 +196,9 @@ if ($bla) {
    * 
    */
   public function queueStatus($queue) {
-    
+
     // list(name, jobs, consumers) = queue_declare(queue=queuename, passive=True)
-    
+
   }
-  
+
 }
