00cfcc65771c8f42f04b0af0cb02253adfb98fb6
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-02-26 10:29:54 -0500

    Explains the need for the Drupal autoload file

--- MessageBroker.php
+++ MessageBroker.php
@@ -1,6 +1,23 @@
 <?php
 /**
- * Entry point for Message Broker PHP Library
+ * Drupal entry point for Message Broker PHP Library. This is solution for the
+ * rather half baked Libraries solution common in Drupal installs that does
+ * not use autoload resulting in libraries dependencies (php-amqplib) not being
+ * loaded.
+ *
+ * As a work around, define hook_libraries_info() to point to this file rather
+ * than MessageBrokerObjectLibrary.php. The composer.json config file ignores
+ * this file allowing for autoload calls.
+ *
+ *   $libraries['messagebroker-phplib'] = array(
+ *    'name' => 'Message Broker PHP Library',
+ *    'vendor url' => 'https://github.com/DoSomething/messagebroker-phplib',
+ *    'download url' => 'https://github.com/DoSomething/messagebroker-phplib',
+ *    'version' => '1.0',
+ *    'files' => array(
+ *      'php' => array('MessageBroker.php'),
+ *    ),
+ * );
  */
 
 // Setup autoloader - loads AMPQ and Mandrill classes
